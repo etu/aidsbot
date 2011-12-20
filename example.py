45725760a10d7d76921c47f9776cccb80d9316c6
Elis Axelsson <elis.axelsson@gmail.com>
2011-12-20 22:40:14 +0100

    Fixed up codestandards

--- example.py
+++ example.py
@@ -7,28 +7,28 @@ def hello(irc,data):
     '''A simple trigger handler function'''
     user_info, msg_type, channel, message = irc.privmsg_split(data)
     username, real_user, host = irc.user_split(user_info)
-    irc.privmsg(channel, "Hello to you too %s :)" % (username))
+    irc.privmsg(channel, 'Hello to you too %s :)' % username)
 
 def join(irc,data):
     '''Handle joins'''
-    print "A user has joined"
+    print 'A user has joined'
 
 def part(irc,data):
-    print "A user has parted"
+    print 'A user has parted'
 
 def postconnect(irc):
     '''Triggers after connect, before joining channels. Good for OPER and other login commands.'''
-    print "Connected to network"
+    print 'Connected to network'
 
-irc = aidsbot('aidsbot-dev', 'irc.oftc.net', 6667, True) #Set up the object
+irc = aidsbot('aidsbot-dev', 'irc.oftc.net', 6667, True) # Set up the object
 irc.postconnect = postconnect # Define the postconnect trigger BEFORE connect
-irc.connect() #Actually connect
+irc.connect() # Actually connect
 irc.join('#bottest') #Join a channel
-irc.privmsg('#bottest', 'Yo!') #Send a message
-irc.privmsghandler_add("!hello", hello) #Add the handlerfunction hello to trigger hello
-irc.chanophandler_add("JOIN",join)
-irc.chanophandler_add("PART",part)
-irc.listen() #Start listening
+irc.privmsg('#bottest', 'Yo!') # Send a message
+irc.privmsghandler_add('!hello', hello) # Add the handlerfunction hello to trigger hello
+irc.chanophandler_add('JOIN',join)
+irc.chanophandler_add('PART',part)
+irc.listen() # Start listening
 
 while True:
-    time.sleep(10) #Important, will die otherwise
+    time.sleep(10) # Important, will die otherwise
